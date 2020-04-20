21cb0a6dc2cbca6b4488d8463780b9c263ca7800
Sreekanth J <sreekanthj93@gmail.com>
2020-04-20 20:02:12 +0530

    Fixing install.sh error outside .dotfiles

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ source $DOTFILES/install/colors
 
 echo -e "\n${LM}Installing dotfiles...${NC}"
 
-source install/link.sh
+source $DOTFILES/install/link.sh
 
 echo -e "\nCreating vim/nvim tmp directories"
 mkdir -p $HOME/.vim-tmp/backup $HOME/.vim-tmp/swap $HOME/.vim-tmp/undo
