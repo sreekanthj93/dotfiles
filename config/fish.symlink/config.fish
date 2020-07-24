6ba8fc185bcf535988efb0ab6786469c82f42c75
Sreekanth J <sreekanthj93@gmail.com>
2020-07-24 11:12:54 +0530

    Fixing fish config PATH variable getting screwed with local bin install locations

--- config/fish.symlink/config.fish
+++ config/fish.symlink/config.fish
@@ -1,20 +1,27 @@
 # load all aliases from .alias.d folder
 for aliasfile in ~/.alias.d/*
-    source $aliasfile
+  source $aliasfile
 end
 
 # load custom alias here if present
 test -s ~/.alias && source ~/.alias || true
 
+# load custom fish config
+test -s ~/.fish_my && source ~/.fish_my || true
+
 # ~/local/bin usages
 if test -d "$HOME/local/bin"
-    export PATH=$HOME/local/bin:$PATH
+  set PATH $HOME/local/bin $PATH
 end
 # ~/local/usr/bin usages
 if test -d "$HOME/local/usr/bin"
-    export PATH=$HOME/local/usr/bin:$PATH
+  set PATH $HOME/local/usr/bin $PATH
 end
 # ~/.local/bin usages
 if test -d "$HOME/.local/bin"
-    export PATH=$HOME/.local/bin:$PATH
+  set PATH $HOME/.local/bin $PATH
+end
+# ~/.local/usr/bin usages
+if test -d "$HOME/.local/usr/bin"
+  set PATH $HOME/.local/usr/bin $PATH
 end
